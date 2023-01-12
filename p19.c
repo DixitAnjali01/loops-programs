c38c1a4450916d0144aab4afb871dac0d9ef5ad7
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 09:31:30 +0530

    Add files via upload

---
+++ p19.c
@@ -0,0 +1,13 @@
+//program to print all ascii characters
+#include <stdio.h>
+int main()
+{
+	int k;// variable declaration   
+for(int k=0;k<=255;k++)// for loop from 0-255  
+	
+    
+    {
+	printf("\nThe ascii value of%cis%d",k,k);
+	}
+	return 0;
+	}
