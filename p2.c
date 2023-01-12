0ef60ed58d779f4fa8f083704f7c86db420a4283
DixitAnjali01 <122504032+DixitAnjali01@users.noreply.github.com>
2023-01-12 09:23:26 +0530

    Add files via upload

---
+++ p2.c
@@ -0,0 +1,14 @@
+//progrm to check if the no. is even or odd
+#include<stdio.h>
+int main(){
+    while(1){
+        int n;
+        printf("Enter number = ");
+        scanf("%d",&n);
+        if(n%2==0){
+            printf("EVEN\n");}
+        else{
+            printf("ODD\n");
+        }
+        }
+    }
